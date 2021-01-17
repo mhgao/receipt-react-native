6aedb77e9b96f309b78b4de171b88a97becc9014
Martin Gao <martin.gao@tufts.edu>
2021-01-17 16:13:33 -0500

    Restructured to create folders components/ and styles; Isolated Camera into components/Camera.js

---
+++ components/Camera.js
@@ -0,0 +1,63 @@
+import React, { PureComponent } from 'react';
+import { StyleSheet, Text, TouchableOpacity, Alert, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+import styles from '../styles/styles';
+
+class Camera extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      takingPic: false,
+    };
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {
+        quality: 0.5,
+        base64: true,
+      };
+      
+
+      try {
+        const data = await this.camera.takePictureAsync(options);
+        console.log(data.uri);
+        // Alert.alert('Success', JSON.stringify(data));
+      } catch (err) {
+        Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
+        return;
+      } finally {
+        this.setState({takingPic: false});
+      }
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          captureAudio={false}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Camera;
